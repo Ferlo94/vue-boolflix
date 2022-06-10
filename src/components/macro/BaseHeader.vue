2e52044ef7bdab8b54fc86102c8c0ce643da85c4
Ferlo94 <100360746+Ferlo94@users.noreply.github.com>
2022-06-10 17:44:50 +0200

    struttura iniziale + axios

---
+++ src/components/macro/BaseHeader.vue
@@ -0,0 +1,60 @@
+<template>
+    <header>
+        <form @submit.prevent="searching">
+            <input type="text" v-model="searchText" required>
+            <button type="submit">CERCA</button>
+        </form>
+    </header>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: 'BaseHeader',
+    data() {
+        return {
+            searchText: '',
+        }
+    },
+
+    methods: {
+        searching () {
+            axios.get('https://api.themoviedb.org/3/search/movie',{
+
+                params: {
+                    api_key: 'e99307154c6dfb0b4750f6603256716d',
+                    query: 'ritorno',
+                    languade: 'it-IT'
+                }
+
+            }).then((response) => {
+            this.films = response.data.results;
+            }).catch((error) => {
+            console.log(error);
+            })
+        }
+    }
+}
+
+
+
+
+</script>
+
+<style>
+
+</style>
+
+
+axios.get('https://api.themoviedb.org/3/search/movie',{
+    params: {
+        api_key: 'e99307154c6dfb0b4750f6603256716d',
+        query: 'ritorno',
+        languade: 'it-IT'
+    }
+}).then((response) => {
+    this.films = response.data.results;
+}).catch((error) => {
+    console.log(error);
+})
