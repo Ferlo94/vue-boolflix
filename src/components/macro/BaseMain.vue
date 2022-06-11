e7b96c323bdabbec4aec8d51fbd59fa570697a22
Ferlo94 <100360746+Ferlo94@users.noreply.github.com>
2022-06-11 14:45:52 +0200

    img aggiunte, flag non funzionante

--- src/components/macro/BaseMain.vue
+++ src/components/macro/BaseMain.vue
@@ -1,11 +1,14 @@
 <template>
   <main>
       <ul>
-          <li v-for="movie in data.movies" :key="movie.id">
+          <li v-for="movie in data.movies" :key="movie.id" :alt="movie.title">
+              
+              <img :src="urlImage(movie.poster_path)">
               <h2>{{movie.title}}</h2>
               <h3>{{movie.original_title}}</h3>
               <p>{{movie.vote_average}}</p>
               <p>{{movie.original_language}}</p>
+              <h4><lang-flag :iso="movie.original_language"/></h4>
           </li>
       </ul>
   </main>
@@ -20,6 +23,16 @@ export default {
         return {
             data,
         }
+    },
+
+    methods: {
+        urlImage(url) {
+            if (url === null) {
+                return `https://via.placeholder.com/185x260`
+            } 
+            return `https://image.tmdb.org/t/p/w185/${url}`
+        },
+     
     }
 }
 </script>
