1b27e4face55c1afbfcf4ef10ca4799beb07b9b0
Ferlo94 <100360746+Ferlo94@users.noreply.github.com>
2022-06-13 17:09:51 +0200

    push che non stampa risultati

---
+++ src/components/commons/BaseCard.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="card">
+            <img :src="urlImage(info.poster_path)">
+            <h2 v-if="info.title">{{info.title}}</h2>
+            <h2 v-else>{{info.name}}</h2>
+            <h3>{{info.original_title ? info.original_title : info.original_name}}</h3>
+            <p>{{info.vote_average}}</p>
+            <p> 
+                <img class="flag" v-if="existFlag(info.original_language)" :src="require(`../../assets/flags/${info.original_language}.png`)" :alt="info.original_language"> 
+                <img class="flag" v-else src="../../assets/flags/lgbt.png" alt="lgbt">
+            </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BaseCard',
+    props: {
+        info : Object,
+    },
+    data() {
+        return {
+            flagsAviable: [
+
+                'en',
+                'ja',
+                'fr',
+                'it',
+                'es',
+                'pt',
+            ]
+        }
+    },
+
+    methods: {
+        existFlag(lang) {
+            return this.flagsAviable.includes(lang);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/macro/BaseMain.vue
+++ src/components/macro/BaseMain.vue
@@ -4,27 +4,14 @@
         <h2>FILMS</h2>
         <ul>
             <li v-for="movie in data.movies" :key="movie.id">
-                <img :src="urlImage(movie.poster_path)">
-                <h2>{{movie.title}}</h2>
-                <h3>{{movie.original_title}}</h3>
-                <p>{{movie.vote_average}}</p>
-                <p> 
-                    <img class="flag" v-if="existFlag(movie.original_language)" :src="require(`../../assets/flags/${movie.original_language}.png`)" :alt="movie.original_language"> 
-                    <img class="flag" v-else src="../../assets/flags/lgbt.png" alt="lgbt">
-                </p>
-            </li>
+                <BaseCard :info="movie"/>
+
+            </li>    
         </ul>
         <h2>SERIE TV</h2>
            <ul>
             <li v-for="tv in data.tv" :key="tv.id">
-                <img :src="urlImage(tv.poster_path)">
-                <h2>{{tv.name}}</h2>
-                <h3>{{tv.original_name}}</h3>
-                <p>{{tv.vote_average}}</p>
-                <p> 
-                    <img class="flag" v-if="existFlag(tv.original_language)" :src="require(`../../assets/flags/${tv.original_language}.png`)" :alt="tv.original_language"> 
-                    <img class="flag" v-else src="../../assets/flags/lgbt.png" alt="lgbt">
-                </p>
+                <BaseCard :info="tv"/>
             </li>
         </ul>
     </main>
@@ -32,21 +19,18 @@
 
 <script>
 import data from '../../shared/data';
+import BaseCard from '../commons/BaseCard.vue';
 
 export default {
     name: 'BaseMain',
+    components: {
+        BaseCard,
+    },
+
     data() {
         return {
             data,
-            flagsAviable: [
-
-                'en',
-                'ja',
-                'fr',
-                'it',
-                'es',
-                'pt',
-            ]
+       
         }
     },
 
@@ -58,9 +42,7 @@ export default {
             } 
             return `https://image.tmdb.org/t/p/w185/${url}`
         },
-        existFlag(lang) {
-            return this.flagsAviable.includes(lang);
-        }
+    
      
     }
 }
